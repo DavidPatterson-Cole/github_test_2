3341c7cd6f9e394d6bb4edef05e976659606c0e3
David Patterson-Cole <dpattersoncole@gmail.com>
2022-10-17 14:49:33 -0400

    Initial Commit

---
+++ github_test_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github_test_2
+//
+//  Created by David Patterson-Cole on 2022-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github_test_2/github_test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  github_test_2App.swift
+//  github_test_2
+//
+//  Created by David Patterson-Cole on 2022-10-17.
+//
+
+import SwiftUI
+
+@main
+struct github_test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
